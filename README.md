8531c6cb743ff9d13eac1e761d69261dafdc08ff
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-10-05 19:55:59 -0500

    Refactoring and bringing all of the developer goodies

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ A place for me to doodle with programming.
 
 # Licensing
 
-Any string in a json file in `docs/content` is licensed under the license found in 
-`LICENSES/CC-BY-SA 4.0.txt.txt`. Everything else is licensed
-under `LICENSES/AGPL-3.0-or-later.txt`.
+- All files and their contents in the `content` directory are licensed under 
+  `LICENSES/CC-BY-SA-4-0.txt`. 
+- All files and their contents in the `docs/scripts` directory are 
+  build artifacts and are **not licensed for use or redistribution**.
+- Everything else not mentioned is licensed under `LICENSES/AGPL-3-0-or-later.txt`.
+
+If you want to use the contents of `docs/scripts`, then please use the AGPL licensed
+code to generate your own with `npm install` and then `npm run compile`.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export interface IState {
-  counter?: number | undefined
+  counter?: number | undefined;
 }
 
 export interface IInput {
@@ -13,13 +13,17 @@ export interface IOutput {
 }
 
 export const world = (props: IInput): IOutput => {
-    if(props?.input === "bob"){
-        return { state: { counter: 1 }, output: "Steven" }
-    } else if(props?.input === "Jerry") {
-        return { state: { counter: 0 }, output: "Enter bob and see Steven, Jerry to return, else be doomed with ipsum." }
-    }
-    return { state: { counter: 10 }, output: lorem }
-}
+  if (props?.input === "bob") {
+    return { state: { counter: 1 }, output: "Steven" };
+  } else if (props?.input === "Jerry") {
+    return {
+      state: { counter: 0 },
+      output:
+        "Enter bob and see Steven, Jerry to return, else be doomed with ipsum.",
+    };
+  }
+  return { state: { counter: 10 }, output: lorem };
+};
 
 const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac lectus in lectus tempor malesuada ac nec augue. Mauris in orci justo. Suspendisse sit amet tempor augue. Duis ultricies placerat tellus, a imperdiet diam pretium vel. Aenean nunc diam, accumsan quis rutrum ac, laoreet vitae ipsum. Vestibulum quis interdum massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet, sem ac aliquam posuere, nisl lorem rhoncus justo, vel rutrum sem lorem mattis ipsum. Mauris lobortis quam tellus, ac eleifend libero fringilla sit amet. Nulla efficitur vestibulum risus facilisis lobortis. Sed aliquam eleifend est in dignissim.
 
@@ -29,4 +33,4 @@ Sed suscipit porta dui, vitae laoreet mauris sagittis quis. Aliquam pellentesque
 
 Ut vel ipsum et tellus elementum hendrerit. Proin auctor quis nibh tempor dictum. Aenean neque lorem, dictum eu enim quis, pulvinar euismod diam. Nunc vel scelerisque mauris. Praesent in augue viverra, suscipit nunc eget, dignissim libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id mauris et erat aliquam porta a vitae metus. Integer tincidunt est vel eleifend consectetur. Praesent vel nulla nec lacus luctus fermentum quis sit amet turpis. Donec turpis augue, ullamcorper non nibh nec, tincidunt vehicula nisl. Curabitur non felis nec quam dapibus ultricies non in diam.
 
-Mauris id mollis nisl, nec condimentum mi. Suspendisse mattis suscipit congue. Cras vitae faucibus ligula, eu cursus erat. Nullam sit amet consequat urna, sit amet venenatis urna. Quisque rhoncus, felis et volutpat consectetur, nulla massa posuere orci, id egestas leo lacus vel augue. Nulla facilisi. Suspendisse commodo quam a malesuada scelerisque. Nulla tortor nibh, molestie et tortor sed, hendrerit consequat lectus. Nam elit odio, condimentum non velit sed, ornare pulvinar sapien.` 
+Mauris id mollis nisl, nec condimentum mi. Suspendisse mattis suscipit congue. Cras vitae faucibus ligula, eu cursus erat. Nullam sit amet consequat urna, sit amet venenatis urna. Quisque rhoncus, felis et volutpat consectetur, nulla massa posuere orci, id egestas leo lacus vel augue. Nulla facilisi. Suspendisse commodo quam a malesuada scelerisque. Nulla tortor nibh, molestie et tortor sed, hendrerit consequat lectus. Nam elit odio, condimentum non velit sed, ornare pulvinar sapien.`;
