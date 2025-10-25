7ba80f8c56874362c85ca53f937acd53b49d9497
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-10-25 13:02:54 -0500

    Reverting

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { 
-    files: ["./src/**/*.{ts}"],
+    files: [".src/**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js }, 
     extends: ["js/recommended"], 
     languageOptions: { globals: globals.browser } 
